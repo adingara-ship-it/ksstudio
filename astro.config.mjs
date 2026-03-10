8bde0ebc540e498b99d733ea48df935aa7ab3731
adingara-ship-it <adingara@gmail.com>
2026-03-10 14:15:36 +0100

    fix: use vercel adapter for astro deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,7 +8,5 @@ export default defineConfig({
 	devToolbar: {
 		enabled: false
 	},
-	adapter: node({
-		mode: 'standalone'
-	})
+	adapter: vercel()
 });
